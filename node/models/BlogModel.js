0bb5ee49b1f5f39e9e8b2ca21b24549a75b1908a
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-04 22:22:18 -0600

    database

---
+++ node/models/BlogModel.js
@@ -0,0 +1,12 @@
+// Forma sequelize.define
+// import db connection
+import db from "../database/db";
+// Import sequelize
+import { DataTypes } from "sequelize"
+
+const BlogModel = db.define("blogs",{
+    title: {type: DataTypes.STRING},
+    content: {type: DataTypes.STRING},
+})
+
+export default
