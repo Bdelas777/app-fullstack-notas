86aac339a6714f1c4cad87dc5ab5775e94ce00dd
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-04 22:28:32 -0600

    database

---
+++ node/database/db.js
@@ -0,0 +1,6 @@
+import { Sequelize } from "sequelize";
+//Database, root, password and object(host and dialect)
+const db = new Sequelize("database_app", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
