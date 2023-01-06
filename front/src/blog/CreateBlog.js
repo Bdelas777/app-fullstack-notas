0bc3d7200d43de047ae51ef24d16ad4ba6318dea
Bdelas777 <93608793+Bdelas777@users.noreply.github.com>
2023-01-05 23:20:09 -0600

    arreglos

--- front/src/blog/CreateBlog.js
+++ front/src/blog/CreateBlog.js
@@ -1,47 +1,49 @@
-import axios from 'axios'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const URI = 'http://localhost:8000/blogs//'
+const URI = "http://localhost:8000/blogs//";
 
 const CompCreateBlog = () => {
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
-    const navigate = useNavigate()    
-    
-    //procedimiento guardar
-    const store = async (e) => {
-        e.preventDefault()
-        await axios.post(URI, {title: title, content:content})
-        navigate('/')
-    }   
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const navigate = useNavigate();
 
-    return (
-        <div>
-           <h3>Create POST</h3>
-           <form onSubmit={store}>
-                <div className='mb-3'>
-                    <label className='form-label'>Title</label>
-                    <input
-                        value={title}
-                        onChange={ (e)=> setTitle(e.target.value)} 
-                        type="text"
-                        className='form-control'
-                    />
-                 </div>   
-                 <div className='mb-3'>
-                     <label className='form-label'>Title</label>
-                    <textarea
-                        value={content}
-                        onChange={ (e)=> setContent(e.target.value)} 
-                        type="text"
-                        className='form-control'
-                    />                 
-                 </div>  
-                 <button type='submit' className='btn btn-primary'>Store</button>                  
-           </form>
+  //procedimiento guardar
+  const store = async (e) => {
+    e.preventDefault();
+    await axios.post(URI, { title: title, content: content });
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <h3>Crea la clase</h3>
+      <form onSubmit={store}>
+        <div className="mb-3">
+          <label className="form-label">Titulo</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            type="text"
+            className="form-control"
+          />
         </div>
-    )
-}
+        <div className="mb-3">
+          <label className="form-label">Contenido</label>
+          <textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Crea
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default CompCreateBlog
+export default CompCreateBlog;
